4106561a8ebf13dc8de3a48a68614599c8d5a28b
AdminSantrax <jairomt95@gmail.com>
2026-02-08 18:50:41 +0100

    add more dicesç

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 //generate a random number between 1 and the number of faces on the dice
 
 function diceGenerator(){
-    let facesInput = document.getElementById("faces");
-  console.log(facesInput); // should NOT be null
-    let numFaces = Number(facesInput.value);
+    let numberIndex = Number(document.getElementById("faces").value);  
+    let faces = Array.from({ length: numberIndex }, (_, i) => i + 1)
+    let square = document.createElement("div");
+    square.classList.add("square");
+    square.textContent = `D${numberIndex}`;
+     const container = document.getElementById("diceContainer");
+  container.appendChild(square);
+  square.addEventListener("click", () => {
+  const randomIndex = Math.floor(Math.random() * faces.length);
+  const result = faces[randomIndex];
+  document.getElementById("resultText").textContent = `you got a ${result}`;
+});
 
-    
-    let result = Math.floor(Math.random() * numFaces) + 1;
-    console.log(result);
+  return faces;
 
-    let resultText = document.getElementById("resultText")
-    resultText.textContent = result;
 }
